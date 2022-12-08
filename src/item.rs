fcf912dc79e74d6d5035c8c494380a48dc843480
Hjalte Nannestad <hjalte.nannestad@protonmail.com>
2022-12-08 14:44:07 +0100

    fix pixelated

--- src/item.rs
+++ src/item.rs
@@ -20,7 +20,7 @@ pub fn ItemView(props: &ItemViewProps) -> Html {
     let mut style = Style::new();
 
     if props.item.pixelated {
-        style.set("filter", "grayscale(100%)");
+        style.set("image-rendering", "pixelated");
     }
 
     html! {
